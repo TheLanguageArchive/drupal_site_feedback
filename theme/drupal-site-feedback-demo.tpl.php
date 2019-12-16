d5b94a1ab2684fd36ab4e47587a6d6f2e2ab77e4
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2019-12-16 17:41:52 +0100

    adding install hook

--- theme/drupal-site-feedback-demo.tpl.php
+++ theme/drupal-site-feedback-demo.tpl.php
@@ -1,4 +1,4 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<drupal-site-feedback url="http://localhost/flat/ds_feedback/295" width="300"></drupal-site-feedback>
+<drupal-site-feedback url="<?php echo $url; ?>" width="300"></drupal-site-feedback>
