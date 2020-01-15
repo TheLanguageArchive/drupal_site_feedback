ceead3a168de651d56e225c6c0d096796212ec10
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2020-01-15 14:17:48 +0100

    fixing multiple elements

--- app/package-element.js
+++ app/package-element.js
@@ -3,21 +3,14 @@ const concat = require('concat');
 
 (async function build() {
 
-    const files = [
-
-        './dist/drupal-site-feedback/runtime-es2015.js',
-        './dist/drupal-site-feedback/polyfills-es2015.js',
-
-        // './dist/drupal-site-feedback/runtime-es5.js',
-        // './dist/drupal-site-feedback/polyfills-es5.js',
-
-        './dist/drupal-site-feedback/styles-es2015.js',
-        // './dist/drupal-site-feedback/styles-es5.js',
-
-        './dist/drupal-site-feedback/main-es2015.js',
-        // './dist/drupal-site-feedback/main-es5.js'
-    ];
-
     await fs.ensureDir('dist/elements');
-    await concat(files, 'dist/elements/drupal-site-feedback.js');
+    await concat([
+
+        './dist/drupal-site-feedback/runtime.js',
+        './dist/drupal-site-feedback/polyfills.js',
+        './dist/drupal-site-feedback/styles.js',
+        './dist/drupal-site-feedback/scripts.js',
+        './dist/drupal-site-feedback/main.js',
+
+    ], 'dist/elements/drupal-site-feedback.js');
 })();
